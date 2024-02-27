650a8fa4e8e3f8809c17f36623690ae69fa96c90
prafullapal <prafullpal18@gmail.com>
2024-02-28 01:59:27 +0530

    Project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	const [themeMode, setThemeMode] = useState("light");
+
+	const darkTheme = () => {
+		setThemeMode("dark");
+	};
+
+	const lightTheme = () => {
+		setThemeMode("light");
+	};
+
+	useEffect(() => {
+		document.querySelector("html").classList.remove("light", "dark");
+		document.querySelector("html").classList.add(themeMode);
+	}, [themeMode]);
+
+	return (
+		<ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+			<div className="flex flex-wrap min-h-screen items-center">
+				<div className="w-full">
+					<div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+						{/* theme btn */}
+						<ThemeBtn />
+					</div>
+
+					<div className="w-full max-w-sm mx-auto">
+						{/* card */}
+						<Card />
+					</div>
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
